9d8a1c519a04aff0c4d22bf7f4a09672dd5571a7
Suliac <122944617+Sul-iac@users.noreply.github.com>
2025-03-06 06:38:48 +0100

    Starting ex02

---
+++ ex02/getters.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getters.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qbarron <qbarron@student.42perpignan.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 06:27:03 by qbarron           #+#    #+#             */
+/*   Updated: 2025/03/06 06:36:34 by qbarron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	getNbAccounts( void ){
+	
+}
+
+static int	getTotalAmount( void ){
+}
+
+static int	getNbDeposits( void ){
+	
+}
+
+static int	getNbWithdrawals( void ){
+}
+
+static void	displayAccountsInfos( void ){
+	
+}
---
+++ ex02/utils.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qbarron <qbarron@student.42perpignan.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 06:34:19 by qbarron           #+#    #+#             */
+/*   Updated: 2025/03/06 06:37:04 by qbarron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+
+void	Account::makeDeposit( int deposit ){
+	
+}
+
+bool	Account::makeWithdrawal( int withdrawal ){
+	
+}
+
+int		Account::checkAmount( void ) const{
+	
+}
+
+void	Account::displayStatus( void ) const{
+	
+}
