2fb8df19956b09fd17eeb715ab34ab9d52c04fa8
Suliac <122944617+Sul-iac@users.noreply.github.com>
2025-03-21 20:35:52 +0100

    ex03 module3 started

---
+++ module1/ex03/Weapon.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qbarron <qbarron@student.42perpignan.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 17:10:19 by qbarron           #+#    #+#             */
+/*   Updated: 2025/03/21 15:57:12 by qbarron          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+# include <string>
+
+class Weapon {
+	private:
+		std::string type;
+	public:
+		
+};
+
+#endif
